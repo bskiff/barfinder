c61850f97da7ec9eb95246eeaa570d8628754a42
saasbook <saasbook@saasbook.(none)>
2014-03-02 01:46:38 -0400

    First git push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barfinder::Application.config.secret_token = '5f8643260fec7eb9503983655865f0bfdf31379c7d09736a8b7f52d03bbec14b100fb86e5c9e19b71f38c98bccaed8f1178b5fd920d6b238d7895fcda6eb39eb'
